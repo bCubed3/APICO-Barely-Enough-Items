28bcff9463cd7f8600a814ceb7a383cefb87e5f9
bCubed3 <sf.bcubed@gmail.com>
2022-06-05 01:37:40 -0700

    Update sample_recipes.lua

--- modules/sample_recipes.lua
+++ modules/sample_recipes.lua
@@ -1,17 +1,18 @@
 -- function that defines example recipes
 function define_sample_recipes()
-    -- cw_define_mod(mod_name : string)
+    -- define your mod with cw_define_mod(mod_name : string, tabs_num : int)
     cw_define_mod("modCubed")
     cw_define_mod("foo")
-    -- cw_define_tab(mod_name : string, num_tabs : int)
-    -- if you want three tabs, call this function 3 times
-    cw_define_tabs("modCubed", 2)
-    cw_define_tabs("foo", 5)
-    -- cw_define_recipe({{item1 : string, amt1 : int}, {item2 : string, amt2 : int}, {item3 : string, amt3 : int} or nil}, output_item : string, output_amt : int)
+    -- define recipes with :
+    -- cw_define_recipe({{item1 : string, amt1 : int}, {item2 : string, amt2 : int}, {item3 : string, amt3 : int} or nil},
+    --                  output_item : string,
+    --                  output_amt : int,
+    --                  tab : int,
+    --                  mod : string)
     -- remember to prepend your mod_name if you are using modded items
-    -- please define recipes AFTER DEFINING EVERYTHING ELSE
-    cw_define_recipe({{"log", 5}}, "stone", 1, 1, "modCubed")
-    cw_define_recipe({{"stone", 5}, {"waterproof", 7}, {"planks2", 10}}, "glue", 2, 1, "modCubed")
-    cw_define_recipe({{"glue", 2}, {"stone", 15}}, "canister1", 1, 1, "foo")
-    cw_define_recipe({{"log", 1}}, "log", 2, 2, "modCubed")
+    -- also remember to define recipes AFTER DEFINING EVERYTHING ELSE IN YOUR MOD
+    cw_define_recipe({{item = "log", amount = 5}}, "stone", 1, 1, "modCubed")
+    cw_define_recipe({{item = "stone", amount = 5}, {item = "waterproof", amount = 7}, {item = "planks2", amount = 10}}, "glue", 2, 1, "modCubed")
+    cw_define_recipe({{item = "glue", amount= 2}, {item = "stone", amount = 15}}, "canister1", 1, 1, "foo")
+    cw_define_recipe({{item = "log", amount = 1}}, "log", 2, 2, "modCubed")
 end
